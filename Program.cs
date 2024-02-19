8e70c388f091b63597fe7ce3aef70809180918c5
lyalka69 <l.y.a.l.k.a7.12.7@gmail.com>
2024-02-19 22:59:58 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktikum4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Cyan;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Title = "Практикум 4 - Сакары Анны";
+
+
+            ////Задание 1. Определить, является ли число чётным или нечетным.
+
+            //Console.Write("Введите число: ");
+            //int num = int.Parse(Console.ReadLine());
+            //if (num % 2 == 0)
+            //    Console.WriteLine($"Число {num} - четное");
+            //else if (num % 2 != 0)
+            //    Console.WriteLine($"Число {num} - не четное");
+            //Console.ReadKey();
+
+            ////Задание 2. Определить, равны ли введенные два числа.
+
+            //Console.Write("Введите число: ");
+            //double number = int.Parse(Console.ReadLine());
+            //Console.Write("Введите число: ");
+            //double number2 = int.Parse(Console.ReadLine());
+            //if (number == number2)
+            //    Console.WriteLine($"Число {number} и {number2} - равны");
+            //else if (number != number2)
+            //    Console.WriteLine($"Число {number} и {number2} - не равны");
+            //Console.ReadKey();
+
+            ////Задание 3. Вводятся три числа. Нужно вывести «YES», если все числа равны, в противном случае - «NO»
+
+            //Console.Write("Введите число numb1: ");
+            //int numb1 = int.Parse(Console.ReadLine());
+            //Console.Write("Введите число numb2: ");
+            //int numb2 = int.Parse(Console.ReadLine());
+            //Console.Write("Введите число numb3: ");
+            //int numb3 = int.Parse(Console.ReadLine());
+            //if (numb1 == numb2 & numb1 == numb3 & numb2 == numb3)
+            //    Console.WriteLine("YES");
+            //else if (numb1 != numb2 & numb1 != numb3 & numb2 != numb3)
+            //    Console.WriteLine("NO");
+            //Console.ReadKey();
+
+            ////Задание 4. Дано целое число. Если оно является положительным, то прибавить к нему 1;
+            ////в противном случае, вычесть из него 2. Вывести полученное число на экран.
+
+            //Console.Write("Введите число: ");
+            //int n = int.Parse(Console.ReadLine());
+            //if (n > 0)
+            //    Console.WriteLine(++n);
+            //else if (n < 0)
+            //    Console.WriteLine(n - 2);
+            //else if (n == 0)
+            //    Console.WriteLine("Ошибка");
+            //Console.ReadKey();
+
+
+            ////Задание 5.Определить, является ли треугольник со сторонами a, b, c равносторонним.
+
+            //Console.WriteLine("Введите сторону 'a' треугольника:");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Введите сторону 'b' треугольника:");
+            //int b = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Введите сторону 'c' треугольника:");
+            //int c = int.Parse(Console.ReadLine());
+            //if (a == b & b == c & c == a)
+            //    Console.WriteLine("Треугольник равносторонний");
+            //else
+            //    Console.WriteLine("треугольник не равносторонний");
+            //Console.ReadKey();
+
+
+
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Praktikum4")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Praktikum4")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("85b3d381-65a4-4265-8398-19dc00ce56be")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
